7c52f061afb57ca72a39605d412f39bf8b925048
hamidou <hamidou.sow@hotmail.fr>
2023-05-25 14:40:08 +0200

    responsive

--- cards/cards.js
+++ cards/cards.js
@@ -13,7 +13,7 @@ function createCard() {
         <div class="card-item">${idCard}</div>
         <div class="card-body">
             <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <p class="card-text">Some quick example text to build.</p>
             <button class="btn btn-danger btn-delete" onclick="deleteOneCard(event)">Supprimer</button>
         </div>
     </div>
