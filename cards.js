7379087bc5fbd252bcb306883ac2151b95de53e4
Hamidou.Sow <hamidou.sow@hotmail.fr>
2023-05-25 08:32:09 +0200

    create cards grill an dtable components

---
+++ cards.js
@@ -0,0 +1,27 @@
+const btnAddCard = document.getElementById('btn-add-card');
+let cards = document.getElementsByClassName('card')
+let deleteBtn = document.querySelectorAll('.btn-delete')
+const containerCards = document.getElementById('container-cards');
+
+btnAddCard.addEventListener('click', () => {createCard()})
+let idCard = 0;
+
+function createCard() {
+    idCard++
+    containerCards.innerHTML += `
+    <div id=${idCard} class="card" style="width: 100%;">
+        <div class="card-item">${idCard}</div>
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <button class="btn btn-danger btn-delete" onclick="deleteOneCard(event)">Supprimer</button>
+        </div>
+    </div>
+    `        
+}
+
+function deleteOneCard(event) {
+    let lui = event.target.parentNode.parentNode.id;
+    let elt = document.getElementById(lui)
+    containerCards.removeChild(elt)
+}
---
+++ tab.js
@@ -0,0 +1,69 @@
+let btnAddRow = document.getElementById('btn-plus')
+let btnRemoveRow = document.getElementById('btn-less')
+let tab = document.querySelector('table')
+let tBody = document.querySelector('tbody')
+let tabRows = tab.rows.length
+let btnAddColumn = document.querySelector('#add-column')
+let btnRemoveColumn = document.querySelector('#remove-column')
+
+let cells = tab.rows[0].cells.length;
+
+btnRemoveRow.removeAttribute('disabled')  
+let newRow = document.createElement('tr')
+newRow.setAttribute('style', 'border: 1px solid black;')
+
+for(let i = 0; i < cells; i++) {
+    let newCase = document.createElement('td')
+    newCase.setAttribute('style','border: 1px solid black; height: 20px;')
+    newRow.append(newCase);
+}
+
+tBody.append(newRow)
+
+function addRow() {  
+    btnRemoveRow.removeAttribute('disabled')  
+    let newRow = document.createElement('tr')
+    newRow.setAttribute('style', 'border: 1px solid black;')
+    for(let i = 0; i < cells; i++) {
+        let newCase = document.createElement('td')
+        newCase.setAttribute('style','border: 1px solid black; height: 20px;')
+        newRow.append(newCase);
+    }    
+    tBody.append(newRow)
+}
+
+function removeRow() {
+    if(tBody.rows.length > 1) {
+        tBody.removeChild(tBody.lastChild)
+    } else {
+        btnRemoveRow.setAttribute('disabled', 'disabled')
+    }    
+}
+
+function addColumn() {
+    btnRemoveColumn.removeAttribute('disabled')
+    cells++
+    for(let i = 1; i < tab.rows.length; i++) {
+        let newCol = document.createElement('td')
+        newCol.setAttribute('style', `border: 1px solid black; width: 100px;`)
+        tab.rows[i].append(newCol)
+    }
+}
+
+function removeColumn() {
+    if(cells > 2) {
+        cells--
+        for(let i = 0; i < tab.rows.length-1; i++) {
+            tBody.rows[i].deleteCell(tab.rows[i].cells.length-1)
+        }
+    } else {
+        btnRemoveColumn.setAttribute('disabled', 'disabled')
+    }  
+}
+
+btnAddRow.addEventListener('click', () => { addRow() })
+btnRemoveRow.addEventListener('click', () => { removeRow()})
+
+btnAddColumn.addEventListener('click', () => { addColumn()})
+btnRemoveColumn.addEventListener('click', () => { removeColumn()})
+
